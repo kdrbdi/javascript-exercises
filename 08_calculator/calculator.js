5d142d582a9c351e43b19353361fb9a34a78c187
kdrbdi <badaoui.abk@gmail.com>
2023-08-29 20:52:42 +0100

    Solve exercices

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((acc, curr) => (acc += curr), 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((acc, curr) => (acc *= curr), 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) return 1;
+
+  // obvious and simple solution
+  let total = 1;
+  for (let index = 1; index <= num; index++) {
+    total *= index;
+  }
+  return total;
+
+  // Complicated for nothing, solution:
+  // const arr = (num) => {
+  //   let tempArr = [];
+  //   for (let i = 1; i <= num; i++) {
+  //     tempArr.push(i);
+  //   }
+  //   return tempArr;
+  // };
+  // return arr(num).reduce((acc, curr) => (acc *= curr), 1);
 };
 
 // Do not edit below this line
@@ -29,5 +46,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
